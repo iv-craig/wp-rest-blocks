71e4336cc2ab341d5d9674568b2fba3d8ce2e165
iv-craig <craig.spivack@iv.agency>
2024-05-16 17:57:25 -0400

    Pull in updates from mainline

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,16 +5,10 @@
  * @package Wp_Rest_Blocks
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+$_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', dirname( __DIR__ ) . '/vendor/yoast/phpunit-polyfills' );
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
@@ -23,9 +17,8 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-rest-blocks.php';
+	require dirname( __DIR__ ) . '/wp-rest-blocks.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+Yoast\WPTestUtils\WPIntegration\bootstrap_it();
